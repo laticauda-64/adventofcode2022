ab3d1e570b1a69a5873036fc0f922f2fc909567f
YoannBDL <yoannb64@gmail.com>
2022-12-08 08:54:21 +0100

    First part ok

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { rawInput } from "./input/rawInput";
 
-const puzzleInput: number[][] = rawInput.split("\n\n").map((e) => e.split("\n").map(Number));
+const answer: number = rawInput
+   .split("\n\n")
+   .map((e) => e.split("\n").map(Number))
+   .map((e) => e.reduce((a, b) => a + b))
+   .reduce((a, b) => Math.max(a, b), -Infinity);
 
-function findTheElf(acc: number[], curr: number[]): number[] {
-   let maxValue = 0;
-   if (acc.reduce((a, b) => a + b) < curr.reduce((a, b) => a + b)) {
-      maxValue = curr.reduce((a, b) => a + b);
-   }
-   return [maxValue];
-}
+console.log(answer);
 
-const fatestElf: number[] = puzzleInput.reduce(findTheElf, [0]);
+// function findTheElf(acc: number[], curr: number[]): number[] {
+//    let maxValue = 0;
+//    if (acc.reduce((a, b) => a + b) < curr.reduce((a, b) => a + b)) {
+//       maxValue = curr.reduce((a, b) => a + b);
+//    }
+//    return [maxValue];
+// }
 
-console.log("The Elf carrying the most calories is the elf number " + fatestElf[0]);
+// const fatestElf: number[] = puzzleInput.reduce(findTheElf, [0]);
+
+// console.log("The Elf carrying the most calories is the elf number " + fatestElf[0]);
