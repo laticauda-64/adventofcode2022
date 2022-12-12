b522414e81c60aa68e9edbc28e230b649efdb41f
YoannBDL <yoannb64@gmail.com>
2022-12-12 09:00:28 +0100

    Clean Array of strings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { rawInput } from "./input/rawInput";
 
+const cleanArrayOfStrings: string[] = rawInput
+   .split("\n\n")
+   .map((e) => e.split("\n"))[0]
+   .map((e) => e.replace(" ", ""));
+
 const finalScore = 0;
+
+console.log(cleanArrayOfStrings);
