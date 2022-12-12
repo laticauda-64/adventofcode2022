2f1cdc8c13df768439de7b70c17b44e5bc8d5fa9
YoannBDL <yoannb64@gmail.com>
2022-12-12 16:20:11 +0100

    First steps

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import { rawInput } from "./input/rawInput";
 import { cleanArrayOfStrings } from ".";
+import { divideStringInTwo } from ".";
 
 describe("cleanArrayOfStrings", () => {
    it("should be an array...", () => {
@@ -7,5 +8,19 @@ describe("cleanArrayOfStrings", () => {
    });
    it("should contains string", () => {
       expect(typeof cleanArrayOfStrings[0]).toBe("string");
+   });
+});
+
+describe("divideStringInTwo", () => {
+   it("should divide the given string in strings of same length", () => {
+      expect(typeof divideStringInTwo("ABCABC")[0]).toBe("string");
+      expect(typeof divideStringInTwo("ABCABC")[1]).toBe("string");
+      expect(divideStringInTwo("ABCABC")[0].length).toEqual(divideStringInTwo("ABCABC")[1].length);
+   });
+
+   it("should throw on even string length", () => {
+      expect(() => {
+         divideStringInTwo("ABCDE");
+      }).toThrow("Every bags should be even !!");
    });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import { rawInput } from "./input/rawInput";
 
 export const cleanArrayOfStrings: string[] = rawInput.split("\n\n").map((e) => e.split("\n"))[0];
 
+export const divideStringInTwo = (bag: string): string[] => {
+   if (bag.length % 2 !== 0) {
+      throw new Error("Every bags should be even !!");
+   }
+
+   const bagSize: number = bag.length / 2;
+
+   const firstHalf = bag.slice(0, bagSize);
+   const secondHalf = bag.slice(bagSize, bag.length);
+
+   return [firstHalf, secondHalf];
+};
+
 const finalScore = 0;
 
-console.log(finalScore);
+console.log(divideStringInTwo("ABCABC"));
