bb0e3309e9bb7957b408ef67de69f5bf57aae8e4
YoannBDL <yoannb64@gmail.com>
2022-12-12 14:32:11 +0100

    Firsts tests & solutions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,10 @@
 import { rawInput } from "./input/rawInput";
 import { cleanArrayOfStrings } from ".";
+import { calculateRowScore } from ".";
+
+const firstStrategy = `A Y
+B X
+C Z`;
 
 describe("cleanArrayOfStrings", () => {
    it("should be an array...", () => {
@@ -7,5 +12,20 @@ describe("cleanArrayOfStrings", () => {
    });
    it("should contains string", () => {
       expect(typeof cleanArrayOfStrings[0]).toBe("string");
+   });
+});
+
+describe("calculateRowScore", () => {
+   it("should return 8", () => {
+      const round = "A Y";
+      expect(calculateRowScore(round)).toEqual(8);
+   });
+   it("should return 1", () => {
+      const round = "B X";
+      expect(calculateRowScore(round)).toEqual(1);
+   });
+   it("should return 6", () => {
+      const round = "C Z";
+      expect(calculateRowScore(round)).toEqual(6);
    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import { rawInput } from "./input/rawInput";
 
-export const cleanArrayOfStrings: string[] = rawInput
-   .split("\n\n")
-   .map((e) => e.split("\n"))[0]
-   .map((e) => e.replace(" ", ""));
+export const cleanArrayOfStrings: string[] = rawInput.split("\n\n").map((e) => e.split("\n"))[0];
+
+export const calculateRowScore = (round: string): number => {
+   // Memo
+   // ABC for opponent, XYZ current player
+   // A,X for Rock, B,Y for Paper, and C,Z for Scissors
+   // Score 1 for Rock, 2 for Paper, and 3 for Scissors
+   // Score 0 for lost, 3 for draw, and 6 for won
+
+   switch (round) {
+      case "A X":
+         return 1 + 3;
+      case "A Y":
+         return 2 + 6;
+      case "A Z":
+         return 3 + 0;
+      case "B X":
+         return 1 + 0;
+      case "B Y":
+         return 2 + 3;
+      case "B Z":
+         return 3 + 6;
+      case "C X":
+         return 1 + 6;
+      case "C Y":
+         return 2 + 0;
+      case "C Z":
+         return 3 + 3;
+      default:
+         return 0;
+   }
+};
 
 const finalScore = 0;
 
